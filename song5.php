190e9897bb92ee2b1ce521f61cee4c0aef6b880f
MendrosMarinel <mendrosmarinel@gmail.com>
2022-04-19 15:12:23 +0800

    php song 5 added

---
+++ song5.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+        <body style = "background-image:repeating-linear-gradient(95deg, rgb(50, 150, 220),rgb(60, 240, 170),black)";>
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music LUV </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music LUV </h1>
+                <p class="lead">Music LUV is the perfect way to learn keynotes and definitely related for those who fell inlove, felt hurt or wants to move on. </p>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                     <div class = "container">
+                        <img src="images/images5.jpg" alt="..." width="180" class="rounded mb-2 img-thumbnail">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Abcdefu Angrier</h2>";
+                        echo "<p class='lead '>Gayle</p>";
+                        echo "<br/>";
+
+
+                        echo "<p> [INTRO]</p>";
+                        echo "<h5><pre> E G#m C#m A</h5>";
+                        echo "<br/>";
+
+                        echo "<p> [VERSE 1]</p>";
+                        echo "<h5><pre> E                            G#m</h5>";
+                        echo "<p> I swear I meant to mean the best when it ended</p>";
+                        echo "<h5><pre> C#m                     A</h5>";
+                        echo "<p> Even tried to bite my tongue when you start shit</p>";
+                        echo "<h5><pre> E                          G#m</h5>";
+                        echo "<p> Now you're textin' all my friends asking questions</p>";
+                        echo "<h5><pre> C#m                         A</h5>";
+                        echo "<p> They never even liked you in the first place</p>";
+                        echo "<h5><pre> E                    G#m</h5>";
+                        echo "<p> Dated a girl that I hate for the attention</p>";
+                        echo "<h5><pre> C#m                   A</h5>";
+                        echo "<p> She only made it two days, what a connection</p>";
+                        echo "<h5><pre> E                       G#m</h5>";
+                        echo "<p> It's like you'd do anything for my affection</p>";
+                        echo "<h5><pre> C#m                       A/h5>";
+                        echo "<p> You're goin' all about it in the worst ways</p>";
+                        echo "<br/>";
+
+                        echo "<p> [PRE-CHORUS]</p>";
+                        echo "<h5><pre> E        G#m             C#m</h5>";
+                        echo "<p> I was into you, but I'm ovеr it now</p>";
+                        echo "<h5><pre> A                  E</h5>";
+                        echo "<p> And I was tryin' to be nice</p>";
+                        echo "<h5><pre> G#m                         C#m</h5>";
+                        echo "<p> But nothing's getting through, so lеt me spell it out</p>";
+                        echo "<br/>";
+
+                        echo "<p> [CHORUS]</p>";
+                        echo "<h5><pre> N.C.            E</h5>";
+                        echo "<p> A-B-C-D-E, Fuck you</p>";
+                        echo "<h5><pre> G#</h5>";
+                        echo "<p> And your mom and your sister and your job</p>";
+                        echo "<h5><pre> C#m                    A</h5>";
+                        echo "<p> And your broke-ass car and that shit you call art</p>";
+                        echo "<h5><pre> E                              G#</h5>";
+                        echo "<p> Fuck you and your friends that I'll never see again</p>";
+                        echo "<h5><pre> C#m                        A</h5>";
+                        echo "<p> Everybody but your dog, you can all fuck off</p>";
+                        echo "<br/>";
+
+                        echo "<p> [POST-CHORUS]</p>";
+                        echo "<h5><pre> E          G#m            C#m</h5>";
+                        echo "<p> Na-na, na-na, na, na, na-na</p>";
+                        echo "<h5><pre> A        E</h5>";
+                        echo "<p> A-B-C-D-E, Fuck you</p>";
+                        echo "<br/>";
+
+                       
+                        echo "<p> [VERSE 2]</p>";
+                        echo "<h5><pre> E                        G#m</h5>";
+                        echo "<p> You said you just needed space and so I gave it</p>";
+                        echo "<h5><pre> C#m                    A   N.C</h5>";
+                        echo "<p> When I had nothin' to say you couldn't take it</p>";
+                        echo "<h5><pre> E                    G#m</h5>";
+                        echo "<p> Told everyone I'm a bitch, so I became it</p>";
+                        echo "<h5><pre> C#m                     A</h5>";
+                        echo "<p> Always had to put yourself above me</p>";
+                        echo "<br/>";
+
+                        echo "<p> [PRE-CHORUS]</p>";
+                        echo "<h5><pre> E        G#m             C#m</h5>";
+                        echo "<p> I was into you, but I'm ovеr it now</p>";
+                        echo "<h5><pre> A                  E</h5>";
+                        echo "<p> And I was tryin' to be nice</p>";
+                        echo "<h5><pre> G#m                         C#m</h5>";
+                        echo "<p> But nothing's getting through, so lеt me spell it out</p>";
+                        echo "<br/>";
+                     
+                        echo "<p> [CHORUS]</p>";
+                        echo "<h5><pre> A           E</h5>";
+                        echo "<p> A-B-C-D-E, Fuck you</p>";
+                        echo "<h5><pre> G#</h5>";
+                        echo "<p> And your mom and your sister and your job</p>";
+                        echo "<h5><pre> C#m                    A</h5>";
+                        echo "<p> And your craigslist couch and the way your voice soundst</p>";
+                        echo "<h5><pre> E                              G#</h5>";
+                        echo "<p> Fuck you and your friends that I'll never see again</p>";
+                        echo "<h5><pre> C#m                        A</h5>";
+                        echo "<p> Everybody but your dog, you can all fuck off</p>";
+                        echo "<br/>";
+
+                        echo "<p> [POST-CHORUS]</p>";
+                        echo "<h5><pre> E          G#m            C#m</h5>";
+                        echo "<p> Na-na, na-na, na, na, na-na</p>";
+                        echo "<h5><pre> A        E</h5>";
+                        echo "<p> A-B-C-D-E, Fuck off</p>";
+                        echo "<h5><pre> E          G#m            C#m</h5>";
+                        echo "<p> Na-na, na-na, na, na, na-na</p>";
+                        echo "<h5><pre> A            E</h5>";
+                        echo "<p> A-B-C-D-E, Fuck you</p>";
+                        echo "<br/>";
+
+                        echo "<p> And your mom and your sister and your job</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> And your broke-ass car and that shit you call art</p>";
+                        echo "<h5><pre> E                              G#m</h5>";
+                        echo "<p> Fuck you and your friends that I'll never see again</p>";
+                        echo "<h5><pre> C#m                        A   N.C.</h5>";
+                        echo "<p> Everybody but your dog, you can all fuck off</p>";
+                       
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Marinel Mendros</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
