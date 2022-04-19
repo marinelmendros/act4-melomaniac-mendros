9fe0f0a4d6e386339e66c782b840e63da63ecef5
MendrosMarinel <mendrosmarinel@gmail.com>
2022-04-19 14:37:05 +0800

    song added

--- index.php
+++ index.php
@@ -9,20 +9,21 @@
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
       
         <link href="css/styles.css" rel="stylesheet" />
+        <body style = "background-image:repeating-linear-gradient(95deg, rgb(50, 150, 220),rgb(60, 240, 170),black)";>
     </head>
     <body id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="#page-top"> Music LUV </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
-                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
-                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
                     </ul>
                 </div>
             </div>
@@ -30,26 +31,124 @@
         <!-- Header-->
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
-                
+                <h1 class="fw-bolder">Welcome to Music LUV </h1>
+                <p class="lead">Music LUV is the perfect way to learn keynotes and definitely related for those who fell inlove, felt hurt or wants to move on. </p>
             </div>
         </header>
         <!-- About section-->
         <section id="songs">
-            <div class="container px-4 ">
+            <div class="container px-4">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-primary">Song Title here</h2>
-                        <p class="lead">display the Composer here</p>
-                        <p>display the lyrics/chords here</p>
+                     <div class = "container">
+                        <img src="images/images0.jpg" alt="..." width="180" class="rounded mb-2 img-thumbnail">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>It Might Be You</h2>";
+                        echo "<p class='lead '>Stephen Bishop</p>";
+                        echo "<br/>";
+
+
+                        echo "<p> [INTRO]</p>";
+                        echo "<h5><pre> G  G/B  C  D</h5>";
+                        echo "<br/>";
+
+                        echo "<p> [VERSE]</p>";
+                        echo "<h5><pre> G</h5>";
+                        echo "<p> Time </p>";
+                        echo "<h5><pre> G/B                     C</h5>";
+                        echo "<p> I've been passing time watching trains go by</p>";
+                        echo "<h5><pre> D             G </h5>";
+                        echo "<p> All of my life</p>";
+                        echo "<h5><pre> G/B                     C</h5>";
+                        echo "<p> Lying on the sand, watching seabirds fly</p>";
+                        echo "<h5><pre> A         G</h5>";
+                        echo "<p> Wishing there would be</p>";
+                        echo "<h5><pre> B7              Em         A</h5>";
+                        echo "<p> Someone waiting home for me</p>";
+                        echo "<br/>";
+
+                        echo "<p> [CHORUS]</p>";
+                        echo "<h5><pre> Am7                     D</h5>";
+                        echo "<p> Something's telling me it might be you</p>";
+                        echo "<h5><pre> Am7                     D</h5>";
+                        echo "<p> Yeah, it's telling me it might be you</p>";
+                        echo "<br/>";
+
+                        echo "<p> [BRIDGE]</p>";
+                        echo "<h5><pre> B  Em                        Bm</h5>";
+                        echo "<p> So many quiet walks to take</p>";
+                        echo "<h5><pre> Dm7                 Am7</h5>";
+                        echo "<p> So many dreams to wake</p>";
+                        echo "<h5><pre> Cm              G</h5>";
+                        echo "<p> And we've so much love to make</p>";
+                        echo "<h5><pre> Em                 A              Dmaj7</h5>";
+                        echo "<p> I think we're gonna need some time</p>";
+                        echo "<h5><pre> Dm7              Cmaj7</h5>";
+                        echo "<p> Maybe all we need is time</p>";
+                        echo "<h5><pre> Am7                    D</h5>";
+                        echo "<p> And it's telling me it might be you</p>";
+                        echo "<h5><pre> G</h5>";
+                        echo "<p> All of my life</p>";
+                        echo "<br/>";
+
+                        echo "<p> [INSTRUMENTAL]</p>";
+                        echo "<h5><pre> G/B  C  D</h5>";
+                        echo "<br/>";
+
+                        echo "<p> [VERSE]</p>";
+                        echo "<h5><pre> G          G/B                   C</h5>";
+                        echo "<p> I've been saving love songs and lullabies</p>";
+                        echo "<h5><pre> A            G</h5>";
+                        echo "<p> And there's so much more</p>";
+                        echo "<h5><pre> B7              Em           A</h5>";
+                        echo "<p> No one's ever heard before</p>";
+                        echo "<br/>";
+
+                       
+                        echo "<p> [CHORUS]</p>";
+                        echo "<h5><pre> Am7                    D</h5>";
+                        echo "<p> Something's telling me it might be you</p>";
+                        echo "<h5><pre> Am7                  D</h5>";
+                        echo "<p> Yeah, it's telling me it must be you</p>";
+                        echo "<h5><pre> Am7                   D</h5>";
+                        echo "<p> And I'm feeling it'll just be you</p>";
+                        echo "<h5><pre> G</h5>";
+                        echo "<p> All of my life</p>";
+                        echo "<br/>";
+
+                        echo "<p> [OUTRO]</p>";
+                        echo "<h5><pre> G/B</h5>";
+                        echo "<p> It's you</p>";
+                        echo "<h5><pre> C</h5>";
+                        echo "<p> It's you</p>";
+                        echo "<h5><pre> D</h5>";
+                        echo "<p> I've been waiting for</p>";
+                        echo "<h5><pre> G</h5>";
+                        echo "<p> All of my life</p>";
+                        echo "<h5><pre> G/B</h5>";
+                        echo "<p> Maybe it's you</p>";
+                        echo "<h5><pre> C</h5>";
+                        echo "<p> Maybe it's you</p>";
+                        echo "<h5><pre> D</h5>";
+                        echo "<p> I've been waiting for</p>";
+                        echo "<h5><pre> G</h5>";
+                        echo "<p> All of my life</p>";
+                        echo "<br/>";                  
+                        
+                       
+
+                        ?>
+
+
                     </div>
                 </div>
             </div>
         </section>
 
-        <footer class="py-3 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add your fullname here</p></div>
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Marinel Mendros</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
