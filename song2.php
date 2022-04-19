38be7b7269bdb8f7aa618d95f62b6ed9cec28047
MendrosMarinel <mendrosmarinel@gmail.com>
2022-04-19 14:58:03 +0800

    php song 2 added

---
+++ song2.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+        <body style = "background-image:repeating-linear-gradient(95deg, rgb(50, 150, 220),rgb(60, 240, 170),black)";>
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music LUV </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music LUV </h1>
+                <p class="lead">Music LUV is the perfect way to learn keynotes and definitely related for those who fell inlove, felt hurt or wants to move on. </p>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                     <div class = "container">
+                        <img src="images/images2.jpg" alt="..." width="180" class="rounded mb-2 img-thumbnail">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Nang Dumating Ka</h2>";
+                        echo "<p class='lead '>Bandang Lapis</p>";
+                        echo "<br/>";
+
+
+                        echo "<p> [INTRO]</p>";
+                        echo "<h5><pre> C G Am F G (2x)</h5>";
+                        echo "<br/>";
+
+                        echo "<p> [VERSE]</p>";
+                        echo "<h5><pre> C          G                      Am</h5>";
+                        echo "<p> Sa araw-araw na gusto kang laging makitay</p>";
+                        echo "<h5><pre> F                     C</h5>";
+                        echo "<p> Nasasabik sa 'yong paglalambing</p>";
+                        echo "<h5><pre> G                        Am</h5>";
+                        echo "<p> 'Pag 'di ka nakikita ang puso ko'y nanghihina</p>";
+                        echo "<h5><pre> F                        C</h5>";
+                        echo "<p> Na para bang 'pag wala ka'y wala na ring saysay</p>";
+                        echo "<h5><pre> G               Am   F G</h5>";
+                        echo "<p> Ang buhay kong ito oh oh oh Oh oh oh...</p>";
+                        echo "<br/>";
+
+                        echo "<p> [PRE-CHORUS]</p>";
+                        echo "<h5><pre> F</h5>";
+                        echo "<p> 'Wag ka lang umalis</p>";
+                        echo "<h5><pre> G</h5>";
+                        echo "<p> 'Wag ka lang lumayo</p>";
+                        echo "<h5><pre> F               G</h5>";
+                        echo "<p> Dito ka lang sa aking tabi</p>";
+                        echo "<br/>";
+
+                        echo "<p> [CHORUS]</p>";
+                        echo "<h5><pre> C                 G</h5>";
+                        echo "<p> Nang dumating ka sa buhay ko</p>";
+                        echo "<h5><pre> Am</h5>";
+                        echo "<p> Binago mong lahat</p>";
+                        echo "<h5><pre> F            G</h5>";
+                        echo "<p> Pati ang aking mundo</p>";
+                        echo "<h5><pre> C</h5>";
+                        echo "<p> Binigyan mo ng ngiti</p>";
+                        echo "<h5><pre> G</h5>";
+                        echo "<p> At ligaya ang buhay</p>";
+                        echo "<h5><pre> Am</h5>";
+                        echo "<p> Pangako ko sa 'yo</p>";
+                        echo "<h5><pre> F</h5>";
+                        echo "<p> Na hindi kita iiwan</p>";
+                        echo "<br/>";
+
+                        echo "<p> [INSTRUMENTAL]</p>";
+                        echo "<h5><pre> C G Am F G</h5>";
+                        echo "<br/>";
+
+                        echo "<p> [VERSE 2]</p>";
+                        echo "<h5><pre> C                   G                  Am</h5>";
+                        echo "<p> Kapag ika'y lumalapit ako'y natutulala sa 'yong</p>";
+                        echo "<h5><pre> F                    C</h5>";
+                        echo "<p> Magagandang ngiti sa akin</p>";
+                        echo "<h5><pre> G                    Am</h5>";
+                        echo "<p> At sana'y mapakinggan mo ang awitin kong 'to</p>";
+                        echo "<h5><pre> F                     C</h5>";
+                        echo "<p> Iisa lang ang pangarap ko sa mundong ito</p>";
+                        echo "<h5><pre> G                Am</h5>";
+                        echo "<p> Ang makasama ka sa araw-araw</p>";
+                        echo "<h5><pre> F           G</h5>";
+                        echo "<p> At makapiling ka sa habang buhay</p>";
+                        echo "<br/>";
+
+                        echo "<p> [PRE-CHORUS]</p>";
+                        echo "<h5><pre> F</h5>";
+                        echo "<p> 'Wag ka lang umalis ('wag ka lang umalis)</p>";
+                        echo "<h5><pre> G</h5>";
+                        echo "<p> 'Wag ka lang lumayo ('wag ka lang lumayo)</p>";
+                        echo "<h5><pre> F                  G</h5>";
+                        echo "<p> Dito ka lang sa aking tabi</p>";
+                        echo "<br/>";
+                        
+                        echo "<p> [CHORUS]</p>";
+                        echo "<h5><pre> C                 G</h5>";
+                        echo "<p> Nang dumating ka sa buhay ko</p>";
+                        echo "<h5><pre> Am</h5>";
+                        echo "<p> Binago mong lahat</p>";
+                        echo "<h5><pre> F            G</h5>";
+                        echo "<p> Pati ang aking mundo</p>";
+                        echo "<h5><pre> C</h5>";
+                        echo "<p> Binigyan mo ng ngiti</p>";
+                        echo "<h5><pre> G</h5>";
+                        echo "<p> At ligaya ang buhay</p>";
+                        echo "<h5><pre> Am</h5>";
+                        echo "<p> Pangako ko sa 'yo</p>";
+                        echo "<h5><pre> F                G</h5>";
+                        echo "<p> Na hindi kita iiwan</p>";
+                        echo "<br/>";
+
+                        echo "<p> [OUTRO]</p>";
+                        echo "<h5><pre> C                 G</h5>";
+                        echo "<p> Nang dumating ka sa buhay ko</p>";
+                        echo "<h5><pre> Am</h5>";
+                        echo "<p> Binago mong lahat</p>";
+                        echo "<h5><pre> F            G</h5>";
+                        echo "<p> Pati ang aking mundo</p>";
+                        echo "<h5><pre> C</h5>";
+                        echo "<p> Binigyan mo ng ngiti</p>";
+                        echo "<h5><pre> G</h5>";
+                        echo "<p> At ligaya ang buhay</p>";
+                        echo "<h5><pre> Am</h5>";
+                        echo "<p> Pangako ko sa 'yo</p>";
+                        echo "<h5><pre> F                G</h5>";
+                        echo "<p> Na hindi kita iiwan</p>";
+                        echo "<br/>";
+
+
+                        
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Marinel Mendros</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
