cfcf37690e73e44ed4f569d0dd181a66a4981824
MendrosMarinel <mendrosmarinel@gmail.com>
2022-04-19 14:50:51 +0800

    php song 1 added

---
+++ song1.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+        <body style = "background-image:repeating-linear-gradient(95deg, rgb(50, 150, 220),rgb(60, 240, 170),black)";>
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music LUV </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music LUV </h1>
+                <p class="lead">Music LUV is the perfect way to learn keynotes and definitely related for those who fell inlove, felt hurt or wants to move on. </p>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                     <div class = "container">
+                        <img src="images/images1.jpg" alt="..." width="180" class="rounded mb-2 img-thumbnail">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>With A Smile</h2>";
+                        echo "<p class='lead '>Eraserheads</p>";
+                        echo "<br/>";
+
+
+                        echo "<p> [CHORUS]</p>";
+                        echo "<h5><pre> G        Am</h5>";
+                        echo "<p> Lift your head, baby, don't be scared</p>";
+                        echo "<h5><pre> Bm                         Am - D</h5>";
+                        echo "<p> Of the things that could go wrong along the way</p>";
+                        echo "<h5><pre> G         Am</h5>";
+                        echo "<p> You'll get by with a smile</p>";
+                        echo "<h5><pre> Bm                     Am - D</h5>";
+                        echo "<p> You can't win at everything but you can try</p>";
+                        echo "<br/>";
+
+                        echo "<p> [VERSE]</p>";
+                        echo "<h5><pre> Bb</h5>";
+                        echo "<p> Baby, you don't have to worry</p>";
+                        echo "<h5><pre> Eb</h5>";
+                        echo "<p> 'Coz there ain't no need to hurry</p>";
+                        echo "<h5><pre> Bb                         Eb</h5>";
+                        echo "<p> No one ever said that there's an easy way</p>";
+                        echo "<h5><pre> Bb</h5>";
+                        echo "<p> When they're closing all their doors</p>";
+                        echo "<h5><pre> Eb</h5>";
+                        echo "<p> And they don't want you anymore</p>";
+                        echo "<h5><pre> Bb              D</h5>";
+                        echo "<p> This sounds funny but I'll say it anyway</p>";
+                        echo "<br/>";
+
+                        echo "<p> [CHORUS]</p>";
+                        echo "<h5><pre> G              Am</h5>";
+                        echo "<p> Girl, I'll stay through the bad times</p>";
+                        echo "<h5><pre> Bm                       Am - D</h5>";
+                        echo "<p> Even if I have to fetch you every day</p>";
+                        echo "<h5><pre> G        Am</h5>";
+                        echo "<p> I'll get by if you smile</p>";
+                        echo "<h5><pre> Bm                       Am - D</h5>";
+                        echo "<p> You can never be too happy in this life</p>";
+                        echo "<br/>";
+
+                        echo "<p> [VERSE]</p>";
+                        echo "<h5><pre> Bb</h5>";
+                        echo "<p> In a world where everybody</p>";
+                        echo "<h5><pre> Eb</h5>";
+                        echo "<p> Hates a happy ending story</p>";
+                        echo "<h5><pre> Bb                              Eb - F</h5>";
+                        echo "<p> It's a wonder love can make this world go 'round</p>";
+                        echo "<h5><pre> Bb</h5>";
+                        echo "<p> And don't let it bring you down</p>";
+                        echo "<h5><pre> Eb</h5>";
+                        echo "<p> And turn your face into a frown</p>";
+                        echo "<h5><pre>  Bb                          D</h5>";
+                        echo "<p> You'll get along with a little prayer and a song</p>";
+                        echo "<br/>";
+
+                        echo "<p> [BRIDGE]</p>";
+                        echo "<h5><pre> G - Am - Bm - Am D</h5>";
+                        echo "<p> (To doo doo doo...) Let me hear you sing it</p>";
+                        echo "<h5><pre> G - Am - Bm - Am D</h5>";
+                        echo "<p> (To doo doo doo...)</p>";
+                        echo "<br/>";
+
+                        echo "<p> [VERSE]</p>";
+                        echo "<h5><pre> Bb</h5>";
+                        echo "<p> In a world where everybody</p>";
+                        echo "<h5><pre> Eb</h5>";
+                        echo "<p> Hates a happy ending story</p>";
+                        echo "<h5><pre> Bb                    Eb - F</h5>";
+                        echo "<p> In a world where everybody</p>";
+                        echo "<h5><pre> Bb</h5>";
+                        echo "<p> But don't let it bring you down</p>";
+                        echo "<h5><pre> Eb</h5>";
+                        echo "<p> And turn your face into a frown</p>";
+                        echo "<h5><pre> Bb                            D</h5>";
+                        echo "<p> You'll get along with a little prayer and a song</p>";
+                        echo "<br/>";
+
+                        echo "<p> [CHORUS]</p>";
+                        echo "<h5><pre> G                 Am</h5>";
+                        echo "<p> Lift your head, baby, don't be scared</p>";
+                        echo "<h5><pre> Bm                        Am - D</h5>";
+                        echo "<p> Of the things that could go wrong along the way</p>";
+                        echo "<h5><pre> G                 Am</h5>";
+                        echo "<p> You'll get by with a smile</p>";
+                        echo "<h5><pre> Bm                      Am - D</h5>";
+                        echo "<p> Now it's time to kiss away those tears goodbye</p>";
+                        echo "<br/>";
+
+                        echo "<p> [SOLO]</p>";
+                        echo "<h5><pre> G - Am - Bm - Am D";
+                        echo "<p> (To doo doo doo...)</p>";
+                        echo "<h5><pre> G - Am - Bm - Am D";
+                        echo "<p> (To doo doo doo...)</p>";
+                        echo "<h5><pre> G - Am - Bm - Am D";
+                        echo "<p> (To doo doo doo...)</p>";
+                        echo "<h5><pre> G - Am - Bm - Am D";
+                        echo "<p> (To doo doo doo....with a smile)</p>";
+
+                        
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Marinel Mendros</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
