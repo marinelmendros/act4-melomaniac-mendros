7fa8067ddbb3ad0cf524e26416de9c62fdb71830
MendrosMarinel <mendrosmarinel@gmail.com>
2022-04-19 15:08:09 +0800

    php song 4 added

---
+++ song4.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+        <body style = "background-image:repeating-linear-gradient(95deg, rgb(50, 150, 220),rgb(60, 240, 170),black)";>
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music LUV </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music LUV </h1>
+                <p class="lead">Music LUV is the perfect way to learn keynotes and definitely related for those who fell inlove, felt hurt or wants to move on. </p>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                     <div class = "container">
+                        <img src="images/images4.jpeg" alt="..." width="180" class="rounded mb-2 img-thumbnail">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Ghost</h2>";
+                        echo "<p class='lead '>Justin Bieber</p>";
+                        echo "<br/>";
+
+
+                        echo "<p> [INTRO]</p>";
+                        echo "<h5><pre> | Bm  G    | D  A    | x2</h5>";
+                        echo "<br/>";
+
+                        echo "<p> [VERSE 1]</p>";
+                        echo "<h5><pre> Bm             G                D  A</h5>";
+                        echo "<p> Youngblood thinks there's always tomorrow</p>";
+                        echo "<h5><pre> Bm           G                   D  A</h5>";
+                        echo "<p> I miss your touch on nights when I'm hollow</p>";
+                        echo "<h5><pre> Bm                    G              D  A</h5>";
+                        echo "<p> I know you crossed the bridge that I can't follow</p>";
+                        echo "<br/>";
+
+                        echo "<p> [PRE-CHORUS]</p>";
+                        echo "<h5><pre> Em            F#m     G</h5>";
+                        echo "<p> Since the love that you left is all that I get</p>";
+                        echo "<h5><pre> G    Bm     A</h5>";
+                        echo "<p> I want you to know that</p>";
+                        echo "<br/>";
+
+                        echo "<p> [CHORUS]</p>";
+                        echo "<h5><pre> Bm       G           D</h5>";
+                        echo "<p> If I can't be close to you</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> I'll settle for the ghost of you</p>";
+                        echo "<h5><pre> Bm       G              D  A</h5>";
+                        echo "<p> I miss you more than life</p>";
+                        echo "<h5><pre> Bm      G         D</h5>";
+                        echo "<p> And if you can't be next to me</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> Your memory is ecstasy</p>";
+                        echo "<h5><pre> Bm        G                D</h5>";
+                        echo "<p> I miss you more than life</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> I miss you more than life</p>";
+                        echo "<br/>";
+
+                        echo "<p> [VERSE 2]</p>";
+                        echo "<h5><pre> D  A</h5>";
+                        echo "<p> Youngblood thinks there's always tomorrow</p>";
+                        echo "<h5><pre> Bm            G                      D  A</h5>";
+                        echo "<p> I need more time, but time can't be borrowed</p>";
+                        echo "<h5><pre> Bm                  G                D  A</h5>";
+                        echo "<p> I'd leave it all behind if I could follow</p>";
+                        echo "<br/>";
+
+                       
+                        echo "<p> [PRE-CHORUS]</p>";
+                        echo "<h5><pre> Em            F#m     G</h5>";
+                        echo "<p> Since the love that you left is all that I get</p>";
+                        echo "<h5><pre> G    Bm     A</h5>";
+                        echo "<p> I want you to know that</p>";
+                        echo "<br/>";
+
+                        echo "<p> [CHORUS]</p>";
+                        echo "<h5><pre> Bm       G           D</h5>";
+                        echo "<p> If I can't be close to you</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> I'll settle for the ghost of you</p>";
+                        echo "<h5><pre> Bm       G              D  A</h5>";
+                        echo "<p> I miss you more than life, yeah</p>";
+                        echo "<h5><pre> Bm      G         D</h5>";
+                        echo "<p> And if you can't be next to me</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> Your memory is ecstasy</p>";
+                        echo "<h5><pre> Bm        G                D</h5>";
+                        echo "<p> I miss you more than life</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> I miss you more than life</p>";
+                        echo "<br/>";
+                     
+                        echo "<h5><pre> | Bm  G    | D  A    | x2</h5>";
+                        echo "<br/>";
+
+                        echo "<h5><pre> Bm       G           D</h5>";
+                        echo "<p> If I can't be close to you</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> I'll settle for the ghost of you</p>";
+                        echo "<h5><pre> Bm       G              D  A</h5>";
+                        echo "<p> I miss you more than life</p>";
+                        echo "<h5><pre> Bm      G         D</h5>";
+                        echo "<p> And if you can't be next to me</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> Your memory is ecstasy</p>";
+                        echo "<h5><pre> Bm        G                D</h5>";
+                        echo "<p> I miss you more than life</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> I miss you more than life</p>";
+                        echo "<br/>";
+                       
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Marinel Mendros</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
